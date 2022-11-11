8fcb14f78e158a0eab9b7f903d8bf33a00890a8a
张恩智 <zhangenzhi657@gmail.com>
2022-11-11 14:38:39 +0900

    add cifar100,minist,supervisor.

--- tki/train/factory.py
+++ tki/train/factory.py
@@ -1,11 +1,17 @@
 
 from .cifar10_rl_student import Cifar10RLStudent
+from .cifar100_rl_student import Cifar100RLStudent
+from .mnist_rl_student import MnistRLStudent
+
 from .cifar10_rl_supervisor import Cifar10RLSupervisor
+from .cifar100_rl_supervisor import Cifar100RLSupervisor
+from .mnist_rl_supervisor import MnistRLSupervisor
 
 
 class StudentFactory():
     def __init__(self) -> None:
-        self.student_list = { 'cifar10': Cifar10RLStudent}
+        self.student_list = { 'cifar10': Cifar10RLStudent, 'cifar100':Cifar100RLStudent,
+                             'mnist':MnistRLStudent}
 
     def __call__(self, student_args, supervisor = None, id = 0):
         return self.get_student(student_args=student_args, 
@@ -22,7 +28,8 @@ class StudentFactory():
 
 class SupervisorFactory():
     def __init__(self) -> None:
-        self.supervisor_list = {'cifar10': Cifar10RLSupervisor}
+        self.supervisor_list = {'cifar10': Cifar10RLSupervisor, 'cifar100':Cifar100RLSupervisor,
+                             'mnist':MnistRLSupervisor}
     
     def __call__(self, supervisor_args, student_target='', id = 0):
         return self.get_supervisor(supervisor_args=supervisor_args, student_target=student_target, id=id)
