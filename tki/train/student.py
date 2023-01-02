44bebf51c65483f63b9ccdfc3878d5b2301b808e
张恩智 <zhangenzhi657@gmail.com>
2023-01-02 22:12:45 +0900

    resnet hvd training.

--- tki/train/student.py
+++ tki/train/student.py
@@ -121,7 +121,7 @@ class Student(Trainer):
     def run(self, connect_queue=None, devices='1'):
 
         # set enviroment
-        self._build_enviroment()
+        # self._build_enviroment()
 
         # prepare dataset
         self.train_dataset, self.valid_dataset, self.test_dataset, \
