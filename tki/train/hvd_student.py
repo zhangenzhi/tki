a7d97710df51e8896c59bd3b1531fee356e0815b
张恩智 <zhangenzhi657@gmail.com>
2023-01-02 18:03:48 +0900

    resnet hvd training.

--- tki/train/hvd_student.py
+++ tki/train/hvd_student.py
@@ -37,7 +37,7 @@ class HVDStudent(Student):
         return optimizer
     
     # @tf.function(experimental_relax_shapes=True, experimental_compile=None)
-    def _train_step(self, inputs, labels, first_batch=False):
+    def _train_step(self, inputs, labels, first_batch=False, action=1.0):
         
         with tf.GradientTape() as tape:
             predictions = self.model(inputs)
@@ -52,11 +52,55 @@ class HVDStudent(Student):
         else:
             tape = hvd.DistributedGradientTape(tape)
             grads = tape.gradient(loss, self.model.trainable_variables)
+            grads = [action*g for g in grads]
             self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
         
         self.mt_loss_fn.update_state(loss)
         
         return loss, grads, train_metrics
+    
+    def train_block(self, epoch, train_steps_per_epoch, train_iter, valid_args, valid_iter):
+        with trange(train_steps_per_epoch, desc="Train steps", leave=False) as t:
+            self.mt_loss_fn.reset_states()
+            self.train_metrics.reset_states()
+            for train_step in t:
+                
+                # train
+                data = train_iter.get_next()
+                first_batch = True if epoch*train_steps_per_epoch+train_step == 0 else False
+                train_loss, train_gard, train_metrics = self._train_step(data['inputs'], data['labels'], 
+                                                                         first_batch=first_batch, action=self.action)
+                
+                
+                # valid
+                if hvd.rank() == 0:
+                    if (epoch*train_steps_per_epoch+train_step) % valid_args['valid_gap'] == 0:
+                        
+                        expect_q_values, state = self.supervisor(self.model.trainable_variables)
+                        act_idx = self.policy(expect_q_values, self.id)
+                        action = self.act_space(act_idx)
+                        self.action = action
+                        
+                        valid_loss, valid_metrics = self.valid_block(train_step, valid_args, valid_iter)
+                        
+                        # training knowledge
+                        q_value = self.training_knowledge.update_buffer(state=state,
+                                                            expect_q_values=expect_q_values, 
+                                                            train_loss=train_loss,
+                                                            train_metric=train_metrics,
+                                                            act_grad = train_gard,
+                                                            action=action,
+                                                            act_idx=act_idx,
+                                                            valid_loss=valid_loss, 
+                                                            valid_metric=valid_metrics,
+                                                            step=epoch*train_steps_per_epoch+train_step)
+                        with self.logger.as_default():
+                            tf.summary.scalar("q_value", q_value, step=epoch*train_steps_per_epoch+train_step)
+                            tf.summary.scalar("action",  action, step=epoch*train_steps_per_epoch+train_step)
+                            
+            etr_loss = self.mt_loss_fn.result()
+            etr_metric = self.train_metrics.result()
+            return etr_loss, etr_metric
     
     def train(self):
         
@@ -85,9 +129,9 @@ class HVDStudent(Student):
                 etr_loss, etr_metric= self.train_block(epoch, train_steps_per_epoch, train_iter, valid_args, valid_iter)
                 
                 # test
-                ete_loss, ete_metric = self.test_block(epoch, test_iter)
-                
                 if hvd.rank() == 0:
+                    ete_loss, ete_metric = self.test_block(epoch, test_iter)
+                
                     e.set_postfix(etr_loss=etr_loss.numpy(), etr_metric=etr_metric.numpy(), ete_loss=ete_loss.numpy(), 
                                 ete_metric=ete_metric.numpy(), lr = self.optimizer.learning_rate.numpy())
                     
