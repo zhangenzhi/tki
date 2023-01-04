6dc3a4579329e10355136d5a824fe38cad105b9a
张恩智 <zhangenzhi657@gmail.com>
2023-01-05 02:17:18 +0900

    one flow model

--- tki/train/hvd_supervisor.py
+++ tki/train/hvd_supervisor.py
@@ -4,15 +4,15 @@ from tqdm import trange
 import tensorflow as tf
 from datetime import datetime
 import horovod.tensorflow as hvd
-hvd.init()
 
-from tki.tools.utils import check_mkdir
+from tki.tools.utils import check_mkdir, print_green
 from tki.train.modules.RL.action import weights_augmentation
 from tki.train.supervisor import Supervisor
 
 class HVDSupervisor(Supervisor):
     def __init__(self, supervisor_args, id = 0):
         super(HVDSupervisor, self).__init__(supervisor_args, id = id)
+        hvd.init()
         
     def _build_enviroment(self):
         gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -43,6 +43,7 @@ class HVDSupervisor(Supervisor):
         return logdir
     
     def train(self):
+        
         # parse train loop control args
         train_loop_args = self.args['train_loop']
         train_args = train_loop_args['train']
@@ -73,6 +74,41 @@ class HVDSupervisor(Supervisor):
                     tf.summary.scalar("etr_loss", etr_loss, step=self.id*total_epochs + epoch)
                     tf.summary.scalar("etr_metric", etr_metric, step=self.id*total_epochs + epoch)
 
-        if hvd.rank() == 0:
-            self.model.summary()
-            self.model_save(name="finished")
+        
+        self.model.summary()
+        self.model_save(name="finished")
+    
+    def run(self, keep_train=False, new_students=[]):
+        if hvd.rank() ==0:
+            if keep_train:
+                # prepare dataset
+                print_green("-"*10+"run_keep"+"-"*10)
+                self.new_students = new_students
+                self.train_dataset, self.valid_dataset, self.test_dataset \
+                    = self.dataloader.load_dataset(new_students = new_students)
+                
+                # train
+                self.train()
+                
+            else:
+                # set enviroment
+                print_green("-"*10+"run_init"+"-"*10)
+
+                # prepare dataset
+                self.train_dataset, self.valid_dataset, self.test_dataset, \
+                self.dataloader = self._build_dataset()
+
+                # build optimizer
+                self.optimizer = self._build_optimizer()
+
+                # build losses and metrics
+                self.loss_fn, self.mt_loss_fn, self.mv_loss_fn, self.mtt_loss_fn = self._build_loss_fn()
+                self.train_metrics, self.valid_metrics, self.test_metrics = self._build_metrics()
+                
+                # build weights and writter
+                self.logger = self._build_logger()
+
+                # train
+                self.train()
+                
+            self.id += 1
